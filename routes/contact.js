5bf85c95ae9c438a752f055ce87ae9fad09e0680
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-10-14 11:36:21 +0100

    added css to index page + loader for questionnaire

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var email = 'hello@cosude.com';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('pages/contact');
+    res.render('pages/contact', {email: email});
 });
 
 module.exports = router;
--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+var questionnaireSource = "https://docs.google.com/forms/d/1JaTPSo32obnxqFfHtnih5BC6HfDvP6hhTnkhiGcFFFw/viewform?embedded=true";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('pages/questionnaire');
+    res.render('pages/questionnaire', {questionnaireSource: questionnaireSource});
 });
 
 module.exports = router;
